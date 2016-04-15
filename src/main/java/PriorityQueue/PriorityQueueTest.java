a18f40367fefe7d386940d3fb0d20029a6a564ab
emrebayrm <e.bayram@gtu.edu.tr>
2016-04-15 05:04:41 +0300

    a

--- src/main/java/PriorityQueue/PriorityQueueTest.java
+++ src/main/java/PriorityQueue/PriorityQueueTest.java
@@ -2,7 +2,9 @@ package PriorityQueue;
 
 import PriorityQueue.IntPriority_Queue;
 
+import java.util.ArrayList;
 import java.util.Random;
+
 
 /**
  * Created by emre on 4/14/16.
@@ -11,14 +13,54 @@ public class PriorityQueueTest {
     public static void main(String[] argv)
      {
          Priority_Queue<Integer> queue = new Priority_Queue<Integer>();
-         Random rand = new Random();
-         for (int i = 0; i < 10000; ++i)
-         {
-             queue.offer(rand.nextInt(99999));
-         }
+         ArrayList<Integer> testresult = new ArrayList<Integer>();
 
-         for (int i = 0; i < 10000; ++i){
-             System.out.println(queue.poll());
+         System.out.println("Testing with 10 elem ");
+         long start = System.currentTimeMillis();
+         Test(queue,10);
+         testresult.add(new Integer((int) (System.currentTimeMillis() - start) ));
+
+         System.out.println("Testing with 100 elem ");
+         start = System.currentTimeMillis();
+         Test(queue,100);
+         testresult.add(new Integer((int) (System.currentTimeMillis() - start) ));
+
+         System.out.println("Testing with 1000 elem ");
+         start = System.currentTimeMillis();
+         Test(queue,1000);
+         testresult.add(new Integer((int) (System.currentTimeMillis() - start) ));
+
+         System.out.println("Testing with 10000 elem ");
+         start = System.currentTimeMillis();
+         Test(queue,10000);
+         testresult.add(new Integer((int) (System.currentTimeMillis() - start) ));
+
+         System.out.println("Testing with 100000 elem ");
+         start = System.currentTimeMillis();
+         Test(queue,100000);
+         testresult.add(new Integer((int) (System.currentTimeMillis() - start) ));
+
+         System.out.println();
+         System.out.println("##################################################");
+         System.out.println("\t\t\t\t\t TEST RESULTS");
+         System.out.println("Size \t\tElapsed Time");
+         int size=10;
+         for (Integer integer: testresult) {
+             System.out.printf("%5d\t\t%s ms \n",size,integer.toString());
+             size*=10;
          }
      }
+
+    private static void Test(Priority_Queue queue,int testsize)
+    {
+        Random rand = new Random();
+        for (int i = 0; i < testsize; ++i)
+        {
+            queue.offer(rand.nextInt(99999));
+        }
+
+        for (int i = 0; i < testsize; ++i){
+            queue.poll();
+        }
+    }
 }
